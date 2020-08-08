3e57cc0987eca40d4b34c5102d7ce041674ec864
bschwarz67 <bryan.schwarz564@gmail.com>
2020-08-07 19:11:22 -0500

    defined Songs, Album, Artist classes. Wrote the part of the input processing that parses the input for all of Artist fields except that map of Albums.

--- MusicFileInfo/MusicFileInfo.cpp
+++ MusicFileInfo/MusicFileInfo.cpp
@@ -8,14 +8,30 @@ using namespace std;
 
 int main() {
 	string fileLine;
-	char title[100], time[100], artist[100], album[100], genre[100], track[100];
+	char title[100], time[100], name[100], album[100], genre[100], track[100];
+	int minutes, seconds;
 	ifstream musicFile;
-	map <string, ArtistInfo> artists;
+	map <string, Artist> artists;
+	map <string, Artist>::iterator artistsIt;
+
 	musicFile.open("music.txt");
 	
 	while(getline(musicFile, fileLine)) {
-		sscanf(fileLine.c_str(), "%s %s %s %s %s %s", title, time, artist, album, genre, track);
-		cout << title << endl;
+		sscanf(fileLine.c_str(), "%s %s %s %s %s %s", title, time, name, album, genre, track);
+		artistsIt = artists.find(name);
+		if(artistsIt == artists.end()) {
+			artistsIt = artists.insert(pair<string, Artist>(name, Artist())).first;
+			artistsIt->second.name = name;
+			sscanf(time, "%d:%d", &minutes, &seconds);
+			artistsIt->second.time = 60 * minutes + seconds;
+			cout << artistsIt->second.time << endl;
+					
+		}
+		else {
+			sscanf(time, "%d:%d", &minutes, &seconds);
+			artistsIt->second.time += 60 * minutes + seconds;
+
+		}
 	}
 	
 	
--- MusicFileInfo/MusicFileInfo.h
+++ MusicFileInfo/MusicFileInfo.h
@@ -1,9 +1,28 @@
 #include <string>
+#include <map>
 #include <vector>
 using namespace std;
 
-class ArtistInfo {
+
+class Song {
 	public:
+		string name;
+		int time;
+		
+};
+
+class Album {
+	public: 
+		string name;
+		int time;
+		vector <Song> songs;
+		int nsongs;
+};
+
+class Artist {
+	public:
+		string name;
+		int time;
+		map <string, Album> albums;
 	private:
-		int numberOfSongs;
 };
