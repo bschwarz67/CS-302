56c164b14559b60ab9dbf3ab23085a0fc6cbdaf2
bschwarz67 <bryan.schwarz564@gmail.com>
2020-11-23 00:01:39 -0600

    commenting and making it a bit more object oriented. lab complete.

--- lab_7_TheMazeSolver/MazeSolver.cpp
+++ lab_7_TheMazeSolver/MazeSolver.cpp
@@ -1,10 +1,10 @@
-//TODO comment, change around that location of path maybe?, return value of solve function?, constructor?
+//TODO change around that location of path maybe?, constructor?
 #include <map>
 #include <iostream>
 #include <vector>
-#include <fstream>
 using namespace std;
 
+//object that represents cells in the Maze, id from 0 to r*c -1
 class Cell {
 	public:
 		int id;
@@ -12,89 +12,71 @@ class Cell {
 		map <int, Cell *> adjacentCells; 
 };
 
-
 class Maze {
 	public:
-		map <int, Cell *> cells;
-		multimap <int, int> walls;
+		map <int, Cell *> cells; //hold all the cells with the cell id as the key
+		multimap <int, int> walls; //holds all the coordinates of the walls
+		vector<int> path;
 		int rows;
 		int cols;
-		bool mazeSolve(Cell * current, vector<int> &path);
+		bool mazeSolve(Cell * current);
+		Maze();
 };
 
-bool Maze::mazeSolve(Cell * current, vector<int> &path) {
-	map <int, Cell *>:: iterator cit;
-	if(current->id == (rows * cols) - 1) {
-		for(int i = 0; i < path.size(); i++) {
-			cout << "PATH " << path[i] << endl;
-		}
-		return true;
-	}
-	for(cit = current->adjacentCells.begin(); cit != current->adjacentCells.end(); cit++) {
-		if(cit->second->visited == 0) {
-			cit->second->visited = 1;
-			path.push_back(cit->second->id);
-			if (mazeSolve(cit->second, path)) {
-				return true;
-			}
-			else {
-				cit->second->visited = 0;
-				path.pop_back();
-			}
-		}	
-	}
-	return false;
-}
-
-
-int main() {
+Maze::Maze() {
 	string s;
 	int i, j;
-	Maze m = Maze();
 	Cell * c;
-	vector<int> path;
 	multimap <int, int>::iterator wit;	
 	map <int, Cell *>::iterator cit;
 	map <int, Cell *>::iterator cit2;
-	cin >> s >> m.rows >> s >> m.cols;
-	cout << "ROWS " << m.rows << " COLS " << m.cols << endl;	
+	//read in and output the number of rows and columns in the maze in the format specified
+	cin >> s >> rows >> s >> cols; 
+	cout << "ROWS " << rows << " COLS " << cols << endl;	
+	
+	//insert the coordinates of the walls in the specifications file into a multimap
 	while(cin >> s >> i >> j) {
-		m.walls.insert(pair<int,int>(i, j));
+		walls.insert(pair<int,int>(i, j));
 	}
 	
-	for(wit = m.walls.begin(); wit != m.walls.end(); wit++) {
+	//print the coordinates of all the walls in the format specified
+	for(wit = walls.begin(); wit != walls.end(); wit++) {
 		cout << "WALL " << wit->first << " " << wit->second << endl;
 	}
 	
 
-	for(i = 0; i < m.rows * m.cols; i++) {
+	//populate the cells data structure of the maze with r*c cells numbered 0 to r*c - 1, set visited to false for each cell
+	for(i = 0; i < rows * cols; i++) {
 		c = new Cell();
 		c->id = i;
 		c->visited = 0;
-		m.cells.insert(pair<int, Cell *>(i, c));
+		cells.insert(pair<int, Cell *>(i, c));
 	}
 
-	for(cit = m.cells.begin(); cit != m.cells.end(); cit++) {
-		cit2 = m.cells.find(cit->first - m.cols);
-		if(cit2 != m.cells.end()) {
+	//for every cell in the maze, put into the cells adjacentCells map every cell that should be adjacent to it
+	//as specified by the dimensions of the file
+	for(cit = cells.begin(); cit != cells.end(); cit++) {
+		cit2 = cells.find(cit->first - cols);
+		if(cit2 != cells.end()) {
 			cit->second->adjacentCells.insert(pair<int, Cell*>(cit2->first, cit2->second));
 		}
-		cit2 = m.cells.find(cit->first + m.cols);
-		if(cit2 != m.cells.end()) {
+		cit2 = cells.find(cit->first + cols);
+		if(cit2 != cells.end()) {
 			cit->second->adjacentCells.insert(pair<int, Cell*>(cit2->first, cit2->second));
 		}
-		cit2 = m.cells.find(cit->first - 1);
-		if(cit->first % m.cols != 0) {
+		cit2 = cells.find(cit->first - 1);
+		if(cit->first % cols != 0) {
 			cit->second->adjacentCells.insert(pair<int, Cell*>(cit2->first, cit2->second));
 		}
-		cit2 = m.cells.find(cit->first + 1);
-		if(cit->first % m.cols != m.cols - 1) {
+		cit2 = cells.find(cit->first + 1);
+		if(cit->first % cols != cols - 1) {
 			cit->second->adjacentCells.insert(pair<int, Cell*>(cit2->first, cit2->second));
 		}
 	}
 	
-	for(wit = m.walls.begin(); wit != m.walls.end(); wit++) {
-		cit = m.cells.find(wit->first);
+	//delete the links in the maze between cells that have walls between them as described by the wall coordinates we put into the multimap
+	for(wit = walls.begin(); wit != walls.end(); wit++) {
+		cit = cells.find(wit->first);
 		cit->second->adjacentCells.find(wit->second)->second->adjacentCells.erase(wit->first);
 		cit->second->adjacentCells.erase(wit->second);
 	}
@@ -102,7 +84,7 @@ int main() {
 
 
 	/*
-	for(cit = m.cells.begin(); cit != m.cells.end(); cit++) {
+	for(cit = cells.begin(); cit != cells.end(); cit++) {
 		cout << cit->first << endl << endl;
 		for(cit2 = cit->second->adjacentCells.begin(); cit2 != cit->second->adjacentCells.end(); cit2++) {
 			cout << cit2->first << endl;
@@ -110,28 +92,59 @@ int main() {
 		cout << endl << endl;
 	}
 	*/
-	if(m.cells.size() > 0) {
-		m.cells.find(0)->second->visited = 1;
-		path.push_back(0);
-		bool x = m.mazeSolve(m.cells.find(0)->second, path);
 
+}
+
+bool Maze::mazeSolve(Cell * current) {
+	map <int, Cell *>::iterator cit;
+	
+	if(current->id == 0) {
+		current->visited = 1;
+		path.push_back(0);
 	}
 
+	//if we have reached the end of the maze, output the path taken to get here and return true
+	if(current->id == (rows * cols) - 1) {
+		for(int i = 0; i < path.size(); i++) {
+			cout << "PATH " << path[i] << endl;
+		}
+		return true;
+	}
+
+	//for every cell adjacent to the current cell that hasnt been visited yet, try to go through this cell to get to the end of the maze
+	for(cit = current->adjacentCells.begin(); cit != current->adjacentCells.end(); cit++) {
+		if(cit->second->visited == 0) {
+			cit->second->visited = 1;
+			path.push_back(cit->second->id);
+			
+			//if the solving function returns that we reached the end, return true;
+			if (mazeSolve(cit->second)) {
+				return true;
+			}
+			
+			//if the function did not reach to end of the maze, pretend we havent used this node, take this node out of the path list
+			//and keep looping through the adjacent nodes
+			else {
+				cit->second->visited = 0;
+				path.pop_back();
+			}
+		}	
+	}
+	//if none of the adjacent cells got us to the end of the maze, go back and try an adjacent cell from the cell we were moving from, or if its the first cell,
+	//the maze has no solution
+	return false;
+}
 
 
+int main() {
+	Maze m = Maze();
 
+	//call the function to test if there is a maze to solve
+	if(m.cells.size() > 0) {
+		m.cells.find(0)->second->visited = 1;
+		bool x = m.mazeSolve(m.cells.find(0)->second);
 
-
-
-
-
-
-
-
-
-
-
-
+	}
 
 	return 0;
 }
